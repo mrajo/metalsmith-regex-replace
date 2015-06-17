ce9862c6db41119896734c2c97351e7b2e9b7a40
mrajo <anthony.c@stle.co>
2015-06-17 14:01:32 -0700

    Add support for .json data files

--- src/loader.js
+++ src/loader.js
@@ -5,11 +5,21 @@ var fs = require('fs');
 
 function loadyaml(filepath) {
     try {
-        var doc = yaml.safeLoad(fs.readFileSync(filepath, 'utf8'));
-        return doc;
+        return yaml.safeLoad(fs.readFileSync(filepath, 'utf8'));
     } catch (e) {
         console.error(e);
     }
 }
 
-module.exports = loadyaml;
+function loadjson(filepath) {
+    try {
+        return JSON.parse(fs.readFileSync(filepath, 'utf8'));
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+module.exports = {
+    yaml: loadyaml,
+    json: loadjson
+};
