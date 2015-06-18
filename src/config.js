4b097e0af22e4ee3642dae45469d2aff7e3d865b
mrajo <anthony.c@stle.co>
2015-06-18 14:26:42 -0700

    Change String.endsWith() calls with regex tests to make compatible with node 0.10+

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ var load = require('../src/loader.js');
 function config(params) {
     var cfg = false;
     if ('object' == typeof params) cfg = params;
-    if ('string' == typeof params && (params.endsWith('.yaml') || params.endsWith('.yml'))) cfg = load.yaml(params);
-    if ('string' == typeof params && params.endsWith('.json')) cfg = load.json(params);
+    if ('string' == typeof params && /\.ya?ml$/.test(params)) cfg = load.yaml(params);
+    if ('string' == typeof params && /\.json$/.test(params)) cfg = load.json(params);
     if ('function' == typeof params) cfg = params();
     if (!cfg) throw new Error('Invalid arguments');
     return cfg;
