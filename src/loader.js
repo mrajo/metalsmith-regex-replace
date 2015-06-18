f57e50d2877f8e4176bb773bbc61f947b1c7ee4a
mrajo <anthony.c@stle.co>
2015-06-18 12:35:58 -0700

    Refactored data file loader because I didn't like the look of the format of the module.exports object

--- src/loader.js
+++ src/loader.js
@@ -3,23 +3,22 @@
 var yaml = require('js-yaml');
 var fs = require('fs');
 
-function loadyaml(filepath) {
+var loader = module.exports = {};
+
+loader.yaml = function (filepath) {
     try {
         return yaml.safeLoad(fs.readFileSync(filepath, 'utf8'));
     } catch (e) {
         console.error(e);
     }
-}
+    return false;
+};
 
-function loadjson(filepath) {
+loader.json = function (filepath) {
     try {
         return JSON.parse(fs.readFileSync(filepath, 'utf8'));
     } catch (e) {
         console.error(e);
     }
-}
-
-module.exports = {
-    yaml: loadyaml,
-    json: loadjson
+    return false;
 };
