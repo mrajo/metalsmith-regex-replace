c02f4ccf4d132f7279185b17c0c55f77b5bb4c3b
mrajo <anthony.c@stle.co>
2017-12-13 12:40:47 -0800

    deduping code in loader

--- src/loader.js
+++ src/loader.js
@@ -3,27 +3,14 @@
 const yaml = require('js-yaml')
 const fs = require('fs')
 
-const loadYaml = (filepath) => {
-  try {
-    return yaml.safeLoad(fs.readFileSync(filepath, 'utf8'))
-  } catch (e) {
-    console.error(e)
-  }
-  return false
-}
-
-const loadJson = (filepath) => {
-  try {
-    return JSON.parse(fs.readFileSync(filepath, 'utf8'))
-  } catch (e) {
-    console.error(e)
-  }
-  return false
-}
-
 const loadConfig = (filepath) => {
-  if (/\.ya?ml$/.test(filepath)) return loadYaml(filepath)
-  if (/\.json$/.test(filepath))  return loadJson(filepath)
+  try {
+    const contents = fs.readFileSync(filepath, 'utf8')
+    if (/\.ya?ml$/.test(filepath)) return yaml.safeLoad(contents)
+    if (/\.json$/.test(filepath))  return JSON.parse(contents)
+  } catch (e) {
+    console.error(e)
+  }
   return false
 }
 
